3220fd8ac9d347a911742e65e0edd06e728f9ec8
unknown <jordanjim10@gmail.com>
2022-05-12 17:35:49 -0500

    weno

---
+++ js/principal.js
@@ -0,0 +1,39 @@
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0;i<pacientes.length;i++){
+
+    paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdIMC = paciente.querySelector(".info-imc");
+    var imc = peso / (altura * altura);
+
+
+    pesoEsValido = true;
+    alturaEsValida = true;
+
+    if((peso < 0)||(peso > 1000)){
+        tdIMC.textContent = "Peso incorrecto";
+        pesoEsValido = false;
+        paciente.classList.add("paciente-incorrecto");
+    }
+
+    if((altura < 0)||(altura > 3.0)){
+        tdIMC.textContent = "Altura incorrecta";
+        alturaEsValida = false;
+        paciente.classList.add("paciente-incorrecto");
+    }
+
+    if(pesoEsValido && alturaEsValida){
+        var imc = peso / (altura * altura);
+        tdIMC.textContent = imc.toFixed(2);
+    }
+
+}
+
